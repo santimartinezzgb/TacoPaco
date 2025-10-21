37900242569f2add81e4ee0c8dec267342797e4c
santimartinezzgb <santimgb98@gmail.com>
2025-10-21 22:58:23 +0200

    añadir a git

---
+++ app/src/main/java/com/example/tacopaco/eleccionMesa.java
@@ -0,0 +1,26 @@
+package com.example.tacopaco;
+
+import static com.example.tacopaco.R.*;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class eleccionMesa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.eleccion_mesa);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.eleccionmesa), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
